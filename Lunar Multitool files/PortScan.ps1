04c3410cba953b7193e205b2c18238dfd8dad71a
NideZ60 <79767448+NideZ60@users.noreply.github.com>
2021-05-20 07:44:24 -0400

    Delete Lunar Multitool files directory

--- Lunar Multitool files/PortScan.ps1
+++
@@ -1,4 +0,0 @@
-$IP = read-host "Enter the IP to scan"
-$target = $IP
-"Loading... (This may take a while)"
-22,80,135,139,443,3389,5900 | % {echo ((New-Object net.sockets.tcpclient).connect("$target",$_)) "$target has $_ open"} 2>junk.txt
